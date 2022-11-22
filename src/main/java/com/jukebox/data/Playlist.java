b1f055551549d2c00cc289da2537e442fcdcc0d8
Sangti konyak <ssangtikonyak@gmail.com>
2022-11-23 00:31:33 +0530

    created playlist class and defined 3 attributes

---
+++ src/main/java/com/jukebox/data/Playlist.java
@@ -0,0 +1,11 @@
+package com.jukebox.data;
+
+import java.util.List;
+
+public class Playlist {
+    private String playlistName;
+    private int playlistId;
+    private List<Song>songList;
+
+
+}
