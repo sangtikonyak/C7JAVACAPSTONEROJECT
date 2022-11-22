5019a9d9540581b3063821a11cda85bafd38011f
Sangti konyak <ssangtikonyak@gmail.com>
2022-11-23 00:33:32 +0530

    defined getter and setter method

--- src/main/java/com/jukebox/data/Playlist.java
+++ src/main/java/com/jukebox/data/Playlist.java
@@ -15,4 +15,28 @@ public class Playlist {
 
     public Playlist() {
     }
+
+    public String getPlaylistName() {
+        return playlistName;
+    }
+
+    public void setPlaylistName(String playlistName) {
+        this.playlistName = playlistName;
+    }
+
+    public int getPlaylistId() {
+        return playlistId;
+    }
+
+    public void setPlaylistId(int playlistId) {
+        this.playlistId = playlistId;
+    }
+
+    public List<Song> getSongList() {
+        return songList;
+    }
+
+    public void setSongList(List<Song> songList) {
+        this.songList = songList;
+    }
 }
