3c409b72a9213a72e7efb0520a56a2e94046e4f8
Sangti konyak <ssangtikonyak@gmail.com>
2022-11-23 00:36:54 +0530

    defined constructor

--- src/main/java/com/jukebox/operation/Jukebox.java
+++ src/main/java/com/jukebox/operation/Jukebox.java
@@ -1,5 +1,16 @@
 package com.jukebox.operation;
 
-public class Jukebox {
+import com.jukebox.data.Song;
 
+import java.util.List;
+
+public class Jukebox {
+  private List<Song> allSong;
+
+    public Jukebox(List<Song> allSong) {
+        this.allSong = allSong;
+    }
+
+    public Jukebox() {
+    }
 }
