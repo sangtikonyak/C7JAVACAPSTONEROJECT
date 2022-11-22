4aac9bfbe84b83621001080385dda5a4ae993a9d
Sangti konyak <ssangtikonyak@gmail.com>
2022-11-23 00:28:54 +0530

    Defined getter and setter

--- src/main/java/com/jukebox/data/Song.java
+++ src/main/java/com/jukebox/data/Song.java
@@ -14,4 +14,44 @@ public class Song {
         this.duration = duration;
         this.id = id;
     }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getArtist() {
+        return artist;
+    }
+
+    public void setArtist(String artist) {
+        this.artist = artist;
+    }
+
+    public String getGenre() {
+        return genre;
+    }
+
+    public void setGenre(String genre) {
+        this.genre = genre;
+    }
+
+    public int getDuration() {
+        return duration;
+    }
+
+    public void setDuration(int duration) {
+        this.duration = duration;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
 }
