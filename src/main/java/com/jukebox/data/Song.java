2bf11ce57dfee50620f70fb7611684d725b00092
Sangti konyak <ssangtikonyak@gmail.com>
2022-11-23 00:28:11 +0530

    created Song constructor

--- src/main/java/com/jukebox/data/Song.java
+++ src/main/java/com/jukebox/data/Song.java
@@ -1,5 +1,17 @@
 package com.jukebox.data;
 
 public class Song {
+    private String title;
+    private String artist;
+    private String genre;
+    private int duration;
+    private int id;
 
+    public Song(String title, String artist, String genre, int duration, int id) {
+        this.title = title;
+        this.artist = artist;
+        this.genre = genre;
+        this.duration = duration;
+        this.id = id;
+    }
 }
