fc8f8c5911449737dcbc344fdc778f5c1d2b2481
Sangti konyak <ssangtikonyak@gmail.com>
2022-11-28 20:53:45 +0530

    searchSong method definition added

--- src/main/java/com/jukebox/operation/Jukebox.java
+++ src/main/java/com/jukebox/operation/Jukebox.java
@@ -27,71 +27,42 @@ public class Jukebox {
 
     public void displayAllSong()
     {
-        System.out.println("Id\t\t Title\t\t Artist\t\t Genre\t\t Duration");
+        int count=0;
+        System.out.println("Song No \t Id \t Title");
         for (Song song : allSong) {
-            System.out.println(song.getId()+"\t\t "+song.getTitle()+"\t\t "+song.getArtist()+"\t\t "+
-                    song.getGenre()+"\t\t "+ song.getDuration());
+            System.out.println("\t "+count+"\t\t "+song.getId()+"\t\t "+song.getTitle());
+            count++;
         }
     }
     public void displayFunctions()
     {
-        System.out.println("1.playMusic");
-        System.out.println("2.Create playlist");
-        System.out.println("3.Search For Song");
-        System.out.println("4.Show Existing Playlist");
-        System.out.println("5.Add Song To Playlist");
-        System.out.println("5.Exit");
+        System.out.println("1.Play a song");
+        System.out.println("2.play all song");
+        System.out.println("3.Create playlist");
+        System.out.println("4.Search For Song");
+        System.out.println("5.Show Existing Playlist");
+        System.out.println("6Add Song To Playlist");
+        System.out.println("7.Exit");
     }
-    
-
-    public List<Song> searchSongByArtist(String name)
-    {
-        List<Song> list=new ArrayList<>();
-        for (Song song : allSong) {
-            if(name.equals(song.getArtist()))
-            {
-                list.add(song);
-            }
-        }
-          return list;
-    }
-
-
-   public void searchSongByGenre(String genre)
-   {
-      String getGenre="";
-      int count=0;
-       for (Song song : allSong) {
-           if(genre.equals(song.getGenre()))
-           {
-               System.out.println(count+" "+song.getTitle());
-           }
-
-       }
-
-   }
-
    public String createPlaylist(String playlistName)
    {
            return playlistName;
        }
-       public void searchSong(String name)
+       public List<Song> searchSong(String name)
        {
-           String getSong="";
+           List<Song> searchSong=new ArrayList<>();
+           int count=0;
+           name=name.substring(0,1).toUpperCase() + name.substring(1);
            System.out.println("id\t\tTitle");
            for (Song song : allSong) {
                if (name.equals(song.getGenre()) || name.equals(song.getArtist()) ||
-                       name.equals(song.getTitle()))
-               {
-                   getSong=song.getArtist();
-                   System.out.println(song.getId()+" "+song.getTitle());
-
+                       name.equals(song.getTitle())){
+                   System.out.println(count+" " + song.getId()+" "+song.getTitle());
+                   searchSong.add(song);
+                   count++;
                }
            }
-           if (name.equals(""))
-           {
-               System.out.println("No Result");
-           }
+           return searchSong;
        }
 
    }
