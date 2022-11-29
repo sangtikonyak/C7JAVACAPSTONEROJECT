edfe094db44a2dd0e10484c4708cc24e7da95fb2
Sangti konyak <ssangtikonyak@gmail.com>
2022-11-30 00:01:27 +0530

    getSongMethod and its definition added

--- src/main/java/com/jukebox/dao/SongInPlaylistDAO.java
+++ src/main/java/com/jukebox/dao/SongInPlaylistDAO.java
@@ -1,4 +1,35 @@
 package com.jukebox.dao;
 
+
+
+import com.jukebox.data.Song;
+import com.jukebox.util.Database;
+
+import javax.management.Query;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
+
 public class SongInPlaylistDAO {
+public static List<Integer> getSongId() throws SQLException {
+    List<Integer> integerList=new ArrayList<>();
+    Connection connection= Database.getConnection();
+    Statement statement=connection.createStatement();
+    String query="SELECT SONG_ID FROM SONGINPLAYLIST";
+    ResultSet resultSet= statement.executeQuery(query);
+    while (resultSet.next())
+    {
+        integerList.add(resultSet.getInt(1));
+    }
+    return integerList;
+}
+public List<Song> getPlaylistSong(int playlistId)
+{
+    List<Song> getPlaylistSong=new ArrayList<>();
+
+    return getPlaylistSong;
+}
 }
